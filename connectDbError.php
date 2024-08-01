de0af38ab7ab4f5dd3a15124c87dcbd818f14742
Deokashumba <deokashumba@gmail.com>
2024-08-01 11:57:11 +0300

    Add files via upload

---
+++ connectDbError.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>error connecting to db</title>
+    <link rel="stylesheet" href="../styles/error.css">
+</head>
+
+<body>
+    <section class="error-container">
+        <p class="error-text">There was an error when connecting to database</p>
+    </section>
+</body>
+
+</html>
---
+++ error.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>error | error when submiting data</title>
+    <link rel="stylesheet" href="../styles/reset.css">
+    <link rel="stylesheet" href="../styles/error.css">
+</head>
+
+<body>
+    <section class="error-container">
+        <p class="error-text">There was an error when submtting your data, return to <a href="../page/login.php">Login page</a></p>
+    </section>
+</body>
+
+</html>
